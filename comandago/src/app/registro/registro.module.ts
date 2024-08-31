635f38ce21d26e3650a33d5e3a8ce473fc30ff14
SebVoce <sa.nunezsilva@gmail.com>
2024-08-31 01:07:15 -0400

    Login + DeleteSideMenu

--- comandago/src/app/registro/registro.module.ts
+++ comandago/src/app/registro/registro.module.ts
@@ -4,17 +4,17 @@ import { FormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
-import { FolderPageRoutingModule } from './folder-routing.module';
+import { RegistroPageRoutingModule } from './registro-routing.module';
 
-import { FolderPage } from './folder.page';
+import { RegistroPage } from './registro.page';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    FolderPageRoutingModule
+    RegistroPageRoutingModule
   ],
-  declarations: [FolderPage]
+  declarations: [RegistroPage]
 })
-export class FolderPageModule {}
+export class RegistroPageModule {}
