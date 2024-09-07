92c419edf18fc0acd0694c6932323d2bf24afa29
dyegoip <die.penaloza@duocuc.cl>
2024-09-07 16:33:20 -0400

    input validar

--- comandago/src/app/app-routing.module.ts
+++ comandago/src/app/app-routing.module.ts
@@ -15,10 +15,6 @@ const routes: Routes = [
     path: 'register',
     loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
   },
-  {
-    path: 'register',
-    loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
-  }
 
 ];
 
--- comandago/src/app/login/login.page.ts
+++ comandago/src/app/login/login.page.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, signal, Component, CUSTOM_ELEMENTS_SCHEMA, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, signal, Component, CUSTOM_ELEMENTS_SCHEMA, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
@@ -6,7 +6,6 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { Router } from '@angular/router';
-import { addIcons } from "ionicons";
 
 @Component({
   selector: 'app-login',
@@ -28,15 +27,16 @@ export class LoginPage implements OnInit {
   formularioLogin: FormGroup;
   hide = signal(true);
 
+  @ViewChild('userInput', { static: true }) user!: ElementRef;
+  @ViewChild('passInput', { static: true }) pass!: ElementRef;
+
   constructor(public fb: FormBuilder, public router: Router) {
     this.formularioLogin = this.fb.group({
       'user': new FormControl("", Validators.required),
       'pass': new FormControl("", Validators.required)
     });
-
-    console.log('Usuario:',);
-    console.log('Contraseña:',);
   }
+
   ngOnInit() {}
 
   clickEvent(event: MouseEvent) {
@@ -45,21 +45,21 @@ export class LoginPage implements OnInit {
   }
 
   login() {
-    const user = this.formularioLogin.get('user')?.value;
-    const pass = this.formularioLogin.get('pass')?.value;
-    
-    console.log('Usuario:', this.formularioLogin);
-    console.log('Contraseña:', pass);
-  
-    if (user == 'diego' && pass == 'diego') {
-      console.log('Login exitoso');
-    } else {
-      console.log(`Credenciales incorrectas  - ${user} ${pass} -`);
-    }
+    setTimeout(() => {
+
+      const userValue = this.user.nativeElement.value;
+      const passValue = this.pass.nativeElement.value;
+
+      if (userValue == 'diego' && passValue == '1234') {
+        console.log(`Login Exitoso - ${userValue} ${passValue}`);
+      } else {
+        console.log(`Credenciales incorrectas - ${userValue} ${passValue}`);
+      }
+    },0);
   }
-  
-  goToRegister(){
-    this.router.navigate(['/register'])
+
+  goToRegister() {
+    this.router.navigate(['/register']);
   }
 
 }
